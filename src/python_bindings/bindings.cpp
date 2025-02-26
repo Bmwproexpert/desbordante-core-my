85311dd47a4adb706e53471769597f2d80ac068b
bmwproexpert <st117326@student.spbu.com>
2025-02-26 10:56:04 +0300

    Created bindings of util funcs and algo of validation dd

--- src/python_bindings/bindings.cpp
+++ src/python_bindings/bindings.cpp
@@ -21,6 +21,7 @@
 #include "statistics/bind_statistics.h"
 #include "ucc/bind_ucc.h"
 #include "ucc/bind_ucc_verification.h"
+#include "dd/bind_dd_verification.h"
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -40,7 +41,7 @@ PYBIND11_MODULE(desbordante, module, pybind11::mod_gil_not_used()) {
     for (auto bind_func :
          {BindMainClasses, BindDataTypes, BindFd, BindCfd, BindAr, BindUcc, BindAc, BindOd, BindNd,
           BindFdVerification, BindMfdVerification, BindUccVerification, BindStatistics, BindInd,
-          BindGfdVerification, BindSplit, BindDynamicFdVerification, BindNdVerification}) {
+          BindGfdVerification, BindSplit, BindDynamicFdVerification, BindNdVerification, BindDDVerification}) {
         bind_func(module);
     }
 }
